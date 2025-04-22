eed6f72bb196aa41e41eb0b1cb4c1dbf23979a68
Lorenzo Lopez <l.lopez14@ufromail.cl>
2025-04-22 18:12:48 -0400

    Implementado json

--- src/main/java/org/staygo/Usuario.java
+++ src/main/java/org/staygo/Usuario.java
@@ -1,5 +1,8 @@
 package org.staygo;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -50,5 +53,18 @@ public class Usuario {
 
     public Long getId_usuario() {
         return id_usuario;
+    }
+
+    @JsonCreator
+    public Usuario(@JsonProperty("id_usuario") Long id_usuario,
+                   @JsonProperty("nombre") String nombre,
+                   @JsonProperty("contrasena") String contrasena,
+                   @JsonProperty("rol") Roles rol,
+                   @JsonProperty("reservas") List<Reserva> reservas) {
+        this.id_usuario = id_usuario;
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+        this.rol = rol;
+        this.reservas = reservas;
     }
 }
