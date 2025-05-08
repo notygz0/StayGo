5c902f11da7ad47e7886a0f021a2e1d427e73ef6
Lorenzo Lopez <l.lopez14@ufromail.cl>
2025-05-07 20:45:04 -0400

    removidos metodos y atributos no usados

--- src/main/java/org/staygo/Hotel.java
+++ src/main/java/org/staygo/Hotel.java
@@ -6,41 +6,14 @@ public class Hotel extends Alojamiento {
 
     private int numEstrellas;
     private int numHabitaciones;
-    private List<String> servicios;
 
-    public Hotel() {
-        super(); // Llamar al constructor de la clase base
-    }
 
     public Hotel(String direccion, float precio, String descripcion, int numEstrellas, int numHabitaciones) {
-        super(direccion, precio, descripcion); // Llamar al constructor de la clase base
+        super(direccion, precio, descripcion);
         this.numEstrellas = numEstrellas;
         this.numHabitaciones = numHabitaciones;
     }
 
-    public int getNumEstrellas() {
-        return numEstrellas;
-    }
-
-    public void setNumEstrellas(int numEstrellas) {
-        this.numEstrellas = numEstrellas;
-    }
-
-    public int getNumHabitaciones() {
-        return numHabitaciones;
-    }
-
-    public void setNumHabitaciones(int numHabitaciones) {
-        this.numHabitaciones = numHabitaciones;
-    }
-
-    public List<String> getServicios() {
-        return servicios;
-    }
-
-    public void setServicios(List<String> servicios) {
-        this.servicios = servicios;
-    }
 
     @Override
     public String verDetalles() {
