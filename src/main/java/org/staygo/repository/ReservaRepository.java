6bf356137eeac5db2597774b9613018d0ae1cb47
notygz0 <149032450+notygz0@users.noreply.github.com>
2025-06-03 23:14:46 -0400

    ReservaRepository con consulta para encontrar reserva por usuario y alojamiento

---
+++ src/main/java/org/staygo/repository/ReservaRepository.java
@@ -0,0 +1,15 @@
+package org.staygo.repository;
+
+import org.staygo.entity.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+    @Query("SELECT r FROM Reserva r WHERE r.user.id = :userId AND r.alojamiento.id = :alojamientoId")
+    Optional<Reserva> findByUsuarioIdAndAlojamientoId(@Param("userId") Long userId, @Param("alojamientoId") Long alojamientoId);
+}
