9bebb7e81c0a157eb0fc6218435c41d66481a750
notygz0 <149032450+notygz0@users.noreply.github.com>
2025-06-03 22:58:19 -0400

    clase para inicializar el proyecto en springboot

---
+++ src/main/java/org/staygo/StayGoApplication.java
@@ -0,0 +1,9 @@
+package org.staygo;
+
+import org.springframework.boot.SpringApplication;
+
+public class StayGoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StayGoApplication.class, args);
+    }
+}
