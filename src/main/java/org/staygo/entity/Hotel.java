704ecdd409dcd8a97c20978f6ed2384796ec14fd
notygz0 <149032450+notygz0@users.noreply.github.com>
2025-06-03 22:12:39 -0400

    clase Hotel migrada a entidad JPA con Lombok y herencia de Alojamiento

--- src/main/java/org/staygo/entity/Hotel.java
+++ src/main/java/org/staygo/entity/Hotel.java
@@ -1,44 +1,26 @@
 package org.staygo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 /**
- * clase que representa un hotel dentro del sistema de gestion de alojamientos.
- * esta clase extiende la clase {@link Alojamiento} y agrega atributos especificos relacionados
- * con un hotel, como el numero de estrellas y el numero de habitaciones.
- *
+ * Entidad que representa un hotel en el sistema
+ * Hereda de Alojamiento e incluye número de estrellas y habitaciones
  * @author Lorenzo Lopez
  * @author Felipe Delgado
  */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="Hotel")
 public class Hotel extends Alojamiento {
-
     private int numEstrellas;
     private int numHabitaciones;
-
-    /**
-     * constructor para crear una nueva instancia de un hotel.
-     *
-     * @param direccion la direccion del hotel.
-     * @param precio el precio del hotel por noche.
-     * @param descripcion una descripcion del hotel.
-     * @param numEstrellas el numero de estrellas del hotel.
-     * @param numHabitaciones el numero de habitaciones del hotel.
-     */
-    public Hotel(String direccion, float precio, String descripcion, int numEstrellas, int numHabitaciones) {
-        super(direccion, precio, descripcion);
-        this.numEstrellas = numEstrellas;
-        this.numHabitaciones = numHabitaciones;
-    }
-
-    /**
-     * obtiene los detalles del hotel.
-     * este metodo sobrescribe el metodo {@link Alojamiento#verDetalles()} para devolver los detalles
-     * especificos del hotel, como la direccion, el precio, el numero de estrellas y el numero de habitaciones.
-     *
-     * @return una cadena con los detalles del hotel.
-     */
-    @Override
-    public String verDetalles() {
-        return "Hotel: " + getDireccion() + ", Precio: " + getPrecio() + ", Estrellas: " + numEstrellas + ", Habitaciones: " + numHabitaciones;
-    }
 }
-
-
