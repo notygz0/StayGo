59cae1350ccd116002bf01e511b4378bf1d7eef9
notygz0 <149032450+notygz0@users.noreply.github.com>
2025-06-03 23:31:27 -0400

    ReservaController con endpoint para listar reservas

---
+++ src/main/java/org/staygo/controller/AuthController.java
@@ -0,0 +1,18 @@
+package org.staygo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @PostMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/register")
+    public String register() {
+        return "register";
+    }
+}
