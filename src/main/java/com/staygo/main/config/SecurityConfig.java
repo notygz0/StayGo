062c533e716b9d612a864b36da3b9fbd2b99cfa1
Lorenzo Lopez <l.lopez14@ufromail.cl>
2025-06-19 14:48:24 -0400

    lambda reemplazado por referencia a método

--- src/main/java/com/staygo/main/config/SecurityConfig.java
+++ src/main/java/com/staygo/main/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.staygo.main.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
@@ -11,7 +12,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // desactiva protección CSRF
+                .csrf(AbstractHttpConfigurer::disable) // desactiva protección CSRF
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll() // permite todas las solicitudes
                 );
