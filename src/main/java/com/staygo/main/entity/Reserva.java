9223e1c7b92b2d06f02e60c85853dfeb07dc596e
Lorenzo Lopez <l.lopez14@ufromail.cl>
2025-06-19 15:29:50 -0400

    reserva implements serializable

--- src/main/java/com/staygo/main/entity/Reserva.java
+++ src/main/java/com/staygo/main/entity/Reserva.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
@@ -14,7 +15,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Entity
 @Table(name = "reserva")
-public class Reserva {
+public class Reserva implements Serializable {
     @Id
     @Column(name="id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
