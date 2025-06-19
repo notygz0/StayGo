c5186e4caa307bcc78cb0f809d7c48a3ff320856
Lorenzo Lopez <l.lopez14@ufromail.cl>
2025-06-19 14:48:50 -0400

    import no utilizado removido

--- src/main/java/com/staygo/main/controller/AuthController.java
+++ src/main/java/com/staygo/main/controller/AuthController.java
@@ -4,7 +4,6 @@ import com.staygo.main.dto.LoginRequest;
 import com.staygo.main.dto.RegisterRequest;
 import com.staygo.main.servicio.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
