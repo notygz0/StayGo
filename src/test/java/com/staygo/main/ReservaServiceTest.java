1cf61e10667f38e6d0f2a9c5080b14f755c2d465
Lorenzo Lopez <l.lopez14@ufromail.cl>
2025-06-19 23:25:09 -0400

    chore: removido import no utilizado

--- src/test/java/com/staygo/main/ReservaServiceTest.java
+++ src/test/java/com/staygo/main/ReservaServiceTest.java
@@ -10,7 +10,6 @@ import com.staygo.main.repository.UserRepository;
 import com.staygo.main.servicio.ReservaService;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
 
